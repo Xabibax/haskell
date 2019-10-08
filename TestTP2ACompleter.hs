b8b346bb34ed315d504a12e6f04593ae8eed6903
Xabi <ale23@hotmail.fr>
2019-10-08 13:58:16 +0200

    Made test type generic

--- TestTP2ACompleter.hs
+++ TestTP2ACompleter.hs
@@ -2,8 +2,42 @@ import Test.QuickCheck
 import TP2ACompleter
 import Data.List
 
-test_myZip :: [Int] -> [Int] -> Bool
+-- myDropWhile :: (a -> Bool) -> [a] -> [a]
+
+-- myElem :: Eq a => a -> [a] -> Bool
+
+-- myNotElem :: Eq a => a -> [a] -> Bool
+
+-- myFilter :: (a -> Bool) -> [a] -> [a]
+
+-- mySplitAt :: Int -> [a] -> ([a],[a])
+
+-- myZip :: [a] -> [b] -> [(a,b)] 
+test_myZip :: Ord a => [a] -> [a] -> Bool
 test_myZip xs ys = myZip xs ys == zip xs ys
 
--- test_myZipWith :: (Int -> Int -> Int) -> [Int] -> [Int] -> Bool
+-- myZipWith :: (a -> b -> c) -> [a] -> [b] -> [c] 
+-- test_myZipWith :: Ord a => (a -> a -> a) -> [a] -> [a] -> Bool
 -- test_myZipWith f xs ys = test_myZipWith f xs ys == zipWith f xs ys
+
+-- myCurry :: ((a,b) -> c) -> a -> b -> c
+
+-- myUncurry :: (a -> b -> c) -> (a,b) -> c
+
+-- myZipWith' :: (a -> b -> c) -> [a] -> [b] -> [c] 
+
+-- myUnzip :: [(a,b)] -> ([a],[b])
+
+-- myConcat' :: [[a]] -> [a]
+
+-- myMap' ::  (a -> b) -> [a] -> [b]
+
+-- myOr' ::  [Bool] -> Bool
+
+-- myAny :: (a -> Bool) -> [a] -> Bool
+
+-- myAll :: (a -> Bool) -> [a] -> Bool
+
+-- myProduct :: [Int] -> Int
+
+-- premiers :: [Int]
